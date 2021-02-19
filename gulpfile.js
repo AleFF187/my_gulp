58fb0e0b01cc9fca7a8c826ceaaf82ac7fcffdfe
AleFF187 <1871847@gmail.com>
2021-02-19 17:27:45 +0200

    добавил автопрефиксер

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,35 @@
 // папка готового проекта
-let project_folder = "dist";
+let project_folder = 'dist';
 // папка исходников
-let source_folder = "src";
+let source_folder = 'src';
 
 // пути
 let path = {
   // готовый проект
   build:{
-    html: project_folder + "/",
-    css: project_folder + "/css/",
-    js: project_folder + "/js/",
-    img: project_folder + "/images/",
-    fonts: project_folder + "/fonts/",
+    html: project_folder + '/',
+    css: project_folder + '/css/',
+    js: project_folder + '/js/',
+    img: project_folder + '/images/',
+    fonts: project_folder + '/fonts/',
   },
   // исходники
   src:{
-    html: [source_folder + "/*.html", "!" + source_folder + "/_*.html"],
-    css: source_folder + "/scss/style.scss",
-    js: source_folder + "/js/main.js",
-    img: source_folder + "/images/**/*.{jpg,png,svg,gif,ico,webp}",
-    fonts: source_folder + "/fonts/",
+    html: [source_folder + '/*.html', '!' + source_folder + '/_*.html'],
+    css: source_folder + '/scss/style.scss',
+    js: source_folder + '/js/main.js',
+    img: source_folder + '/images/**/*.{jpg,png,svg,gif,ico,webp}',
+    fonts: source_folder + '/fonts/',
   },
   // наблюдаем
   watch:{
-    html: source_folder + "/**/*.html",
-    css: source_folder + "/scss/**/*.scss",
-    js: source_folder + "/js/**/*.js",
-    img: source_folder + "/images/**/*.{jpg,png,svg,gif,ico,webp}",    
+    html: source_folder + '/**/*.html',
+    css: source_folder + '/scss/**/*.scss',
+    js: source_folder + '/js/**/*.js',
+    img: source_folder + '/images/**/*.{jpg,png,svg,gif,ico,webp}',    
   },
   // удаление готового проекта при запуске галпа
-  clean: "./" + project_folder + "/",
+  clean: './' + project_folder + '/',
 }
 
 let {src, dest} = require('gulp'),
@@ -37,13 +37,14 @@ let {src, dest} = require('gulp'),
     browsersync = require('browser-sync').create(),
     fileinclude = require('gulp-file-include'),
     del = require('del'),
-    scss = require('gulp-sass');
+    scss = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer');
 
 // функция обновления страницы
 function browserSync(){
   browsersync.init({
     server: {
-      baseDir: "./" + project_folder + "/"
+      baseDir: './' + project_folder + '/'
     },
     port: 3000,
     notify: false,
@@ -67,7 +68,14 @@ function css() {
     // обработка scss
     .pipe(
       scss({
-        outputStyle: "expanded"
+        outputStyle: 'expanded'
+      })
+    )
+    // автопрефиксер
+    .pipe(
+      autoprefixer({
+        overrideBrowserslist: ['last 8 versions'],
+        cascade: true
       })
     )
     // перебрасываем файлы в папку с готовым проектом
